6a65002f29dfe21f03dec7198b44fa27a6375ce9
Jonathan Beri <jmberi@gmail.com>
2025-08-16 06:02:18 -0700

    feat: implement dynamic Homebrew formula with version placeholders

--- Formula/usbipd-mac.rb
+++ Formula/usbipd-mac.rb
@@ -1,8 +1,9 @@
 class UsbipDMac < Formula
   desc "macOS USB/IP protocol implementation for sharing USB devices over IP"
   homepage "https://github.com/beriberikix/usbipd-mac"
-  url "https://github.com/beriberikix/usbipd-mac/archive/v#{version}.tar.gz"
-  sha256 "CHECKSUM_PLACEHOLDER"
+  url "https://github.com/beriberikix/usbipd-mac/archive/VERSION_PLACEHOLDER.tar.gz"
+  version "VERSION_PLACEHOLDER"
+  sha256 "SHA256_PLACEHOLDER"
   license "MIT"
   
   depends_on :macos => :big_sur
