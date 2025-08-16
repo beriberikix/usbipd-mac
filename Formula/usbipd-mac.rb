f75a44ce4f62cdd94a5dd1a0338db0fa918aef7d
Jonathan Beri <jmberi@gmail.com>
2025-08-16 05:58:34 -0700

    feat: add initial Homebrew formula structure

---
+++ Formula/usbipd-mac.rb
@@ -0,0 +1,27 @@
+class UsbipDMac < Formula
+  desc "macOS USB/IP protocol implementation for sharing USB devices over IP"
+  homepage "https://github.com/beriberikix/usbipd-mac"
+  url "https://github.com/beriberikix/usbipd-mac/archive/v#{version}.tar.gz"
+  sha256 "CHECKSUM_PLACEHOLDER"
+  license "MIT"
+  
+  depends_on :macos => :big_sur
+  depends_on :xcode => ["13.0", :build]
+  
+  def install
+    system "swift", "build", "--configuration", "release", "--disable-sandbox"
+    bin.install ".build/release/usbipd"
+    bin.install ".build/release/QEMUTestServer"
+  end
+  
+  service do
+    run [opt_bin/"usbipd", "--daemon"]
+    require_root true
+    log_path "/var/log/usbipd.log"
+    error_log_path "/var/log/usbipd.error.log"
+  end
+  
+  test do
+    system "#{bin}/usbipd", "--version"
+  end
+end
